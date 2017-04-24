f45cfd6b4d05f32d5d84cb20f2613711a2217220
Rob Stewart <robstewart57@gmail.com>
2017-04-24 01:41:48 +0100

    Changes graph layout

--- plot_5year_ratio.R
+++ plot_5year_ratio.R
@@ -11,7 +11,7 @@ dat <- read.csv(inputFile)
 ggplot(data=dat, aes(x=year, y=growth, group=placeName, linetype = placeName, colour = placeName)) +
     geom_line(size=1.3) +
     theme_bw() +
-    theme(aspect.ratio=3/3, axis.title=element_text(size=18,face="bold"), axis.text.y = element_text(size=18), axis.text.x = element_text(size=18), legend.title = element_blank()) +
-    guides(linetype = guide_legend(override.aes = list(size=0.8)))
+    theme(aspect.ratio=3/3, axis.title=element_text(size=18,face="bold"), axis.text.y = element_text(size=18), axis.text.x = element_text(size=18), legend.title = element_blank(), legend.text=element_text(size=18)) +
+    guides(linetype = guide_legend(override.aes = list(size=1)))
 
-ggsave(outputFile)
+ggsave(outputFile, width=10, height=10, dpi=100)
