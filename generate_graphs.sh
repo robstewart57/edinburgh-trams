2122b9f3f75c176a4cfb78bdaf60cb67e09499bd
Rob Stewart <robstewart57@gmail.com>
2017-04-24 01:31:24 +0100

    Adds graphs

--- generate_graphs.sh
+++ generate_graphs.sh
@@ -3,7 +3,13 @@
 Rscript plot_5year_ratio.R trams-ratio.csv trams-ratio.pdf
 pdftrimwhite trams-ratio.pdf trams-ratio.pdf
 rm trams-ratio.log
+convert trams-ratio.pdf trams-ratio.png
+mv trams-ratio.png graphs/
+rm trams-ratio.pdf
 
 Rscript plot_5year_ratio.R national-ratio.csv national-ratio.pdf
 pdftrimwhite national-ratio.pdf national-ratio.pdf
 rm national-ratio.log
+convert national-ratio.pdf national-ratio.png
+mv national-ratio.png graphs/
+rm national-ratio.pdf
